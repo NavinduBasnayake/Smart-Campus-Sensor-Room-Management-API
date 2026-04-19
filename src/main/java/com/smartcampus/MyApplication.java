2f4b4789e0dbc81413b4340d367852f6f40a1198
Navindu Basnayake <navindusathsara070@gmail.com>
2026-04-19 17:15:30 +0530

    Add application entry point with API base path /api/v1

---
+++ src/main/java/com/smartcampus/MyApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Navindu Basnayake
+ */
+@ApplicationPath("/api/v1")
+public class MyApplication extends Application {
+}
+
